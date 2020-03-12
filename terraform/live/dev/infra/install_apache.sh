5e22e59b239544220f8ff2bfd9fae3184749cf6d
jupudi roopesh <roopeshjupudi@gmail.com>
2020-03-12 21:03:58 +1300

    updated apache script

--- terraform/live/dev/infra/install_apache.sh
+++ terraform/live/dev/infra/install_apache.sh
@@ -2,5 +2,5 @@
 sudo yum update -y
 sudo yum install httpd -y
 sudo service httpd start
-echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+echo "<h1>Hello Plexure</h1>" | sudo tee /var/www/html/index.html
 sudo service httpd restart
